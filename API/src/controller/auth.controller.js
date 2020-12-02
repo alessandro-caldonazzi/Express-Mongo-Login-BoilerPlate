c2527f89787bed73f3e2dc72609dff86c2fa7b00
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-02 20:04:13 +0100

    ADD: register controller, save user

--- API/src/controller/auth.controller.js
+++ API/src/controller/auth.controller.js
@@ -1,3 +1,17 @@
+const User = require('../models/user.model');
+const mongoose = require('mongoose');
 exports.register = async(req, res, next) => {
+    let { username, password, email } = req.body;
+    const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username,
+        password,
+        email
+    });
 
-}
+
+    user.save().then(result => {
+        console.log(result);
+        res.json({ "ok": true, "data": user });
+    }).catch(err => console.log(err));
+};
