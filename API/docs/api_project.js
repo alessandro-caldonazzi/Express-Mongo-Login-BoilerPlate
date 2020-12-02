b6d4737f8d5cef867b6231c7888be02d06092f8b
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-02 18:31:19 +0100

    DOCS: /auth/register

--- API/docs/api_project.js
+++ API/docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-02T17:25:51.733Z",
+    "time": "2020-12-02T17:30:25.021Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
--- API/src/routes/auth.js
+++ API/src/routes/auth.js
@@ -10,22 +10,12 @@ var router = express.Router();
  * @apiGroup Auth
  * @apiPermission public
  *
- * @apiParam  {String}          email     User's email
+ * @apiParam  {String{3..128}}          username     User's nikname
+ * @apiParam  {String{6..128}}        email     User's email
  * @apiParam  {String{6..128}}  password  User's password
  *
- * @apiSuccess (Created 201) {String}  token.tokenType     Access Token's type
- * @apiSuccess (Created 201) {String}  token.accessToken   Authorization Token
+ * @apiSuccess (Created 201) {String}  token.jwt   Authorization Token (Json Web Token)
  * @apiSuccess (Created 201) {String}  token.refreshToken  Token to get a new accessToken
- *                                                   after expiration time
- * @apiSuccess (Created 201) {Number}  token.expiresIn     Access Token's expiration time
- *                                                   in miliseconds
- * @apiSuccess (Created 201) {String}  token.timezone      The server's Timezone
- *
- * @apiSuccess (Created 201) {String}  user.id         User's id
- * @apiSuccess (Created 201) {String}  user.name       User's name
- * @apiSuccess (Created 201) {String}  user.email      User's email
- * @apiSuccess (Created 201) {String}  user.role       User's role
- * @apiSuccess (Created 201) {Date}    user.createdAt  Timestamp
  *
  * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
  */
