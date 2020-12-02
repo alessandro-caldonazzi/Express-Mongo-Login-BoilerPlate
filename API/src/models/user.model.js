2c5a96195d9e878b430432208a596df023332726
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-02 20:03:36 +0100

    ADD: pre Save function to encrypt password

--- API/src/models/user.model.js
+++ API/src/models/user.model.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     username: String,
     password: String,
     email: String
+});
+
+userSchema.pre('save', async function(next) {
+    try {
+        if (!this.isModified('password')) return next();
+
+        const hash = await bcrypt.hash(this.password, 15);
+        this.password = hash;
+
+        return next();
+    } catch (error) {
+        return next(error);
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
