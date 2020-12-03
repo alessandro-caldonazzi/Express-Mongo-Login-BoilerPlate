6df3599af10cd72a19f9d6b48592cde6c8f807b6
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-03 19:59:25 +0100

    ADD: APIError class

---
+++ API/src/utils/apiError.js
@@ -0,0 +1,19 @@
+class APIError extends Error {
+    constructor({
+        message,
+        errors,
+        status,
+        isPublic,
+        stack,
+    }) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message;
+        this.errors = errors;
+        this.status = status;
+        this.isPublic = isPublic;
+        this.stack = stack;
+    }
+}
+
+module.exports = APIError;
