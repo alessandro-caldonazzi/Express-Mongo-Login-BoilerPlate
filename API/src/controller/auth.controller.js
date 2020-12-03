3c3517868ea62f6281bfa8b4b825e2d85ff4903a
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-03 20:02:44 +0100

    ALTER: error handling in register controller

--- API/src/controller/auth.controller.js
+++ API/src/controller/auth.controller.js
@@ -2,15 +2,18 @@ const User = require('../models/user.model');
 const APIError = require('../utils/apiError');
 const mongoose = require('mongoose');
 exports.register = async(req, res, next) => {
-    let { username, password, email } = req.body;
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username,
-        password,
-        email
-    });
+    try {
+        let { username, password, email } = req.body;
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            username,
+            password,
+            email
+        });
 
-    user.save().then(result => {
-        res.json({ "ok": true, "data": user });
-    }).catch(err => console.log(err));
+        let a = await user.save();
+        console.log(a);
+    } catch (err) {
+        next(User.checkForDuplicateEmail(err));
+    }
 };
