af085170836e7f1e89c39fd2f21e548726fd2bad
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-04 22:48:37 +0100

    ADD: input validation with joi

---
+++ API/src/validator/auth.validator.js
@@ -0,0 +1,36 @@
+const Joi = require('joi');
+
+module.exports = {
+    // POST /auth/register
+    register: {
+        body: Joi.object({
+            username: Joi.string()
+                .required(),
+            email: Joi.string()
+                .email()
+                .required(),
+            password: Joi.string()
+                .required()
+                .min(6)
+                .max(128),
+        }),
+    },
+
+    // POST /auth/login
+    login: {
+        body: Joi.object({
+            username: Joi.string()
+                .required(),
+            password: Joi.string()
+                .required()
+                .max(128),
+        }),
+    },
+
+    // POST /auth/refresh
+    refresh: {
+        body: Joi.object({
+            refreshToken: Joi.string().required().regex(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_.+/=]*$/),
+        }),
+    },
+};
