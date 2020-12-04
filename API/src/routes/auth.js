7b4c966a19e4103e7578787e5100aa69aa8cc5bb
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-04 17:52:42 +0100

    ADD: new /auth/login endpoint

--- API/src/routes/auth.js
+++ API/src/routes/auth.js
@@ -17,8 +17,26 @@ var router = express.Router();
  * @apiSuccess (Created 201) {String}  token.jwt   Authorization Token (Json Web Token)
  * @apiSuccess (Created 201) {String}  token.refreshToken  Token to get a new accessToken
  *
- * @apiError (Bad Request 400)  ValidationError  Some parameters may contain invalid values
+ * @apiError (Bad Request 401)  ValidationError  Some parameters may contain invalid values
  */
 router.post('/register', controller.register);
+
+/**
+ * @api {post} auth/login Login
+ * @apiDescription Login user given username and password
+ * @apiVersion 1.0.0
+ * @apiName Login
+ * @apiGroup Auth
+ * @apiPermission public
+ *
+ * @apiParam  {String{3..128}}          username     User's nikname
+ * @apiParam  {String{6..128}}  password  User's password
+ *
+ * @apiSuccess (Created 201) {String}  token.jwt   Authorization Token (Json Web Token)
+ * @apiSuccess (Created 201) {String}  token.refreshToken  Token to get a new accessToken
+ *
+ * @apiError (Bad Request 401)  ValidationError  Some parameters may contain invalid values
+ */
+router.post('/login', controller.login);
 
 module.exports = router;
