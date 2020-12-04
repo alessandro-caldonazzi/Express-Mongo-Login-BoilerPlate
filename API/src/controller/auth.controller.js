aa84f1a193f8fd528fa9a28b525e397997427683
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-04 18:59:21 +0100

    ADD: login controller

--- API/src/controller/auth.controller.js
+++ API/src/controller/auth.controller.js
@@ -13,11 +13,28 @@ exports.register = async(req, res, next) => {
             email
         });
 
-        let saved = await user.save();
-        let { jwtToken, refreshToken } = await session.newSession({ "_id": saved._id, username, email }, "user");
+        const saved = await user.save();
+        const { jwtToken, refreshToken } = await session.newSession({ "_id": saved._id, username, email }, "user");
 
         res.status(200).json({ jwtToken, refreshToken });
     } catch (err) {
         next(User.checkForDuplicateEmail(err));
+    }
+};
+
+exports.login = async(req, res, next) => {
+    try {
+        let { username, password } = req.body;
+        const user = await User.findUser(username, password);
+
+        const { jwtToken, refreshToken } = await session.newSession({
+            "_id": user._id,
+            "username": user.username,
+            "email": user.email
+        }, "user");
+
+        res.status(200).json({ jwtToken, refreshToken });
+    } catch (err) {
+        next(err);
     }
 };
