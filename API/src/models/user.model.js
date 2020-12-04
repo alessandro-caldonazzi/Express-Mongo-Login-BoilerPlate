4db398016148da53d2722fbdafb89d33448b167c
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-04 18:59:01 +0100

    Upgraded model schema, new method to check password, findUser method

--- API/src/models/user.model.js
+++ API/src/models/user.model.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const APIError = require('../utils/apiError');
+const httpStatus = require('http-status');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         unique: true,
+        required: true,
     }
 });
 
@@ -25,23 +33,41 @@ userSchema.pre('save', async function(next) {
     }
 });
 
+userSchema.method({
+    async checkPassword(password) {
+        return bcrypt.compare(password, this.password);
+    }
+});
 
 userSchema.statics.checkForDuplicateEmail = (error) => {
-        if (error.name === 'MongoError' && error.code === 11000) {
-            //check for duplicate key error
-            return new APIError({
-                message: 'Validation Error',
-                errors: [{
-                    field: 'email',
-                    location: 'body',
-                    messages: ['email already exists'],
-                }],
-                status: 409,
-                isPublic: true,
-                stack: error.stack,
-            });
-        }
-        return error;
-    },
+    if (error.name === 'MongoError' && error.code === 11000) {
+        //check for duplicate key error
+        return new APIError({
+            message: 'Validation Error',
+            errors: [{
+                field: 'email',
+                location: 'body',
+                messages: ['email already exists'],
+            }],
+            status: 409,
+            isPublic: true,
+            stack: error.stack,
+        });
+    }
+    return error;
+};
 
-    module.exports = mongoose.model('User', userSchema);
+userSchema.statics.findUser = async function(username, password) {
+    const err = {
+        status: httpStatus.UNAUTHORIZED,
+        isPublic: true,
+    };
+    if (!username) throw new APIError({...err, message: 'A username is required' });
+    if (!password) throw new APIError({...err, message: 'A password is required' });
+    const user = await this.findOne({ username }).exec();
+    if (!user || !await user.checkPassword(password)) throw new APIError({...err, message: 'Incorrect username or password' });
+    return user;
+};
+
+
+module.exports = mongoose.model('User', userSchema);
