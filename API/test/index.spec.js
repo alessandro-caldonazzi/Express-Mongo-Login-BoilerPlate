a15fbc752fa2311ff1ae2de093d75a14a61c1f8d
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-07 18:56:51 +0100

    Trying to fix travis

--- API/test/index.spec.js
+++ API/test/index.spec.js
@@ -16,6 +16,7 @@ describe('test', () => {
             .post('/auth/register')
             .send({ 'email': 'email@example.com', 'password': 'password', 'username': 'username' })
             .end((err, res) => {
+                console.log(err);
                 res.should.have.status(200);
                 res.body.should.have.property('jwt');
                 res.body.should.have.property('refreshToken');
