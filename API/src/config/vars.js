4aa325a516e9ff2adffb1e48e3f7f4227b686b78
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-08 12:30:15 +0100

    ADD: config/vars.js  for config

---
+++ API/src/config/vars.js
@@ -0,0 +1,8 @@
+module.exports = {
+    emailConfig: {
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        username: process.env.EMAIL_USERNAME,
+        password: process.env.EMAIL_PASSWORD,
+    },
+};
