8fd3a54f2ba1a837bc96eb269b604b3ab0761155
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-08 11:43:10 +0100

    ADD: resetTokenPassword model

---
+++ API/src/models/resetTokenPassword.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+const resetPasswordSchema = new mongoose.Schema({
+    resetToken: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    createdAt: { type: Date, default: Date.now, expires: 60 }
+});
+
+resetPasswordSchema.statics = {
+    async generate(user) {
+        const userId = user._id;
+        const resetToken = `${userId}.${crypto.randomBytes(40).toString('hex')}`;
+        const resetTokenObj = new ResetTokenPassword({
+            resetToken,
+            userId,
+        });
+        await resetTokenObj.save();
+        return resetTokenObj;
+    },
+};
+
+const ResetTokenPassword = mongoose.model('ResetTokenPassword', resetPasswordSchema);
+module.exports = ResetTokenPassword;
