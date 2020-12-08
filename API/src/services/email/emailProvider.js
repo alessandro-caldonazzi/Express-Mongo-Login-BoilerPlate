9640adf90804a80d221c9878e155f8e41c35b2ad
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-08 12:31:12 +0100

    ADD: emailProvider under services/email

---
+++ API/src/services/email/emailProvider.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+const { emailConfig } = require('../../config/vars');
+const Email = require('email-templates');
+
+const transporter = nodemailer.createTransport({
+    port: emailConfig.port,
+    host: emailConfig.host,
+    auth: {
+        user: emailConfig.username,
+        pass: emailConfig.password,
+    },
+    secure: false,
+});
+
+transporter.verify((error) => {
+    if (error) {
+        console.log('error with email connection');
+    }
+});
+
+exports.sendPasswordReset = async(resetTokenObj, address) => {
+    const email = new Email({
+        views: { root: __dirname },
+        message: {
+            from: 'noreply@app.com',
+        },
+        send: true,
+        transport: transporter,
+    });
+
+    email.send({
+            template: 'passwordReset',
+            message: {
+                to: address,
+            },
+            locals: {
+                productName: 'Express-mongo-login',
+                passwordResetUrl: resetTokenObj.resetToken,
+            },
+        })
+        .catch(() => console.log('error sending password reset email'));
+};
