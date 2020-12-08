be351f456131769e594085887d492c4a518054ab
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-08 13:14:48 +0100

    ADD: googleLogin controller structure

--- API/src/controller/auth.controller.js
+++ API/src/controller/auth.controller.js
@@ -71,3 +71,11 @@ exports.resetTokenPassword = async(req, res, next) => {
         next(err);
     }
 };
+
+exports.googleLogin = async(req, res, next) => {
+    try {
+        console.log(req.body);
+    } catch (err) {
+        next(err);
+    }
+};
