cadedf239b145608a02fea36679ea6a738b06bf6
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2020-12-16 16:09:46 +0100

    reset code expires in 2 hours

--- API/src/models/resetTokenPassword.model.js
+++ API/src/models/resetTokenPassword.model.js
@@ -11,7 +11,7 @@ const resetPasswordSchema = new mongoose.Schema({
         ref: 'User',
         required: true,
     },
-    createdAt: { type: Date, default: Date.now, expires: 60 }
+    createdAt: { type: Date, default: Date.now, expires: 60 * 60 * 2 }
 });
 
 resetPasswordSchema.statics = {
