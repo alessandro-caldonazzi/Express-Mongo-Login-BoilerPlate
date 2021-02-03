90c947a5dd1c2eab9d61c4015abfccd69baf18cc
Alessandro Caldonazzi <alessandro.caldonazzi03@gmail.com>
2021-02-03 20:24:48 +0100

    UPDATE: docs

--- API/docs/api_project.js
+++ API/docs/api_project.js
@@ -1,15 +1,26 @@
 define({
-  "name": "example",
+  "name": "Express mongo login boilerplate",
   "version": "1.0.0",
-  "description": "apiDoc basic example",
-  "title": "Custom apiDoc browser title",
-  "url": "https://api.github.com/v1",
+  "description": "A boilerplate for your node API",
+  "title": "A boilerplate for your node API",
+  "url": "/",
+  "order": [
+    "Register",
+    "Login",
+    "Refresh",
+    "Reset password step 1",
+    "Reset password step 2",
+    "Auth - Login",
+    "Define",
+    "PostTitleAndError",
+    "PostError"
+  ],
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-12-13T20:47:12.153Z",
+    "time": "2021-02-03T19:21:34.378Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
